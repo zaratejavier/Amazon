9ec3bb9e5ff7800e80ec4b86985c828fe0fc339c
Javier Zarate <zarate.javier.jz@gmail.com>
2020-09-08 21:32:17 -0600

    added a image to the home component that will be behind all our other info

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from "react"
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2020/FALL_1/GW/DH/AF_M_WATCHES_2x._CB407768965_.jpg" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Home
+
+
+{/* <img alt="Alexa, open Jeopardy | Echo Show" src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MDEyMjRkYzYt/MDEyMjRkYzYt-MWIxMWRkYjMt-w3000._CB406838427_.jpg" height="600px" width="1500px" data-a-hires="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MDEyMjRkYzYt/MDEyMjRkYzYt-MWIxMWRkYjMt-w3000._CB406838427_.jpg"> */ }
+{/* <img alt="Timeless watches for every style" src="https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2020/FALL_1/GW/DH/AF_M_WATCHES_2x._CB407768965_.jpg" height="600px" width="1500px" data-a-hires="https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2020/FALL_1/GW/DH/AF_M_WATCHES_2x._CB407768965_.jpg"></img> */}
