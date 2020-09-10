989904c74a75034a484817cede2ee140d36e49bd
Javier Zarate <zarate.javier.jz@gmail.com>
2020-09-09 18:13:45 -0600

    added react router to be able to navigate to different pages

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,14 @@ import React from "react"
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
   return (
     <div className="header">
-      <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+      <Link to="/">
+        <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+      </Link>
       
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -29,10 +32,12 @@ const Header = (props) => {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header__optionLineTwo header__basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
 
       </div>
     </div>
