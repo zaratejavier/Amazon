730f7073790d59b972452c0d4ef18373961ef1a0
Javier Zarate <zarate.javier.jz@gmail.com>
2020-09-10 16:12:02 -0600

    added the number of items to the basket once you click add to basket

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 const Header = (props) => {
+  const [{ basket }, dispatch] = useStateValue()
+  
   return (
     <div className="header">
       <Link to="/">
@@ -35,7 +38,7 @@ const Header = (props) => {
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
           </div>
         </Link>
 
