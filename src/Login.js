f496fc3d0f0e79770e25358c273d6f3448e7a06f
Javier Zarate <zarate.javier.jz@gmail.com>
2020-09-25 13:13:32 -0600

    added sign in and register function. Next step is to get them working with firebasee

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import './Login.css'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const signIn = (e) => {
+    e.preventDefault()
+
+    //do firebase stuff here for the signin
+  }
+
+  const register = (e) => {
+    e.preventDefault()
+
+    //do firebase stuff here for the register
+  }
+  
   return (
     <div className="login">
       <Link to='/'>
@@ -14,19 +29,28 @@ const Login = () => {
         <h1>Sign-in</h1>
         <form>
           <h5>Email</h5>
-          <input type='text' />
+          <input
+            type='text'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
           
           <h5>Password</h5>
-          <input type='text' />
+          <input
+            type='text'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          
+          />
 
-          <button className='login__signInButton'>Sign in</button>
+          <button type="submit" onClick={signIn} className='login__signInButton'>Sign in</button>
         </form>
 
         <p>
           By signing-in you agree to AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice
         </p>
         
-        <button className='login__registerButton'>Create your Amazon account</button>
+        <button onClick={register} className='login__registerButton'>Create your Amazon account</button>
       </div>
     </div>
     
